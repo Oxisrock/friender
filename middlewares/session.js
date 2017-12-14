b91e51c4e4e40374f235ac1bdfd8c059acd69c75
Orlando J Galicia <orlandoj.galicia@gmail.com>
2017-12-14 17:47:12 -0400

    corrigiendo datos middleware

--- middlewares/session.js
+++ middlewares/session.js
@@ -1,7 +1,7 @@
 const Company = require('../models/company')
 module.exports = function(req, res, next) {
-  if (!req.session.company_id) {
-    res.redirect('/logini')
+  if (req.session.company_id) {
+    res.redirect('/companys/login')
   } else {
     Company.findById(req.session.company_id, function(err, company) {
       if (err) {
